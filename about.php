970c680e9493c86d2ad0587beee39a908392c52e
unknown <corentin.malbrun1@gmail.com>
2022-06-10 14:08:10 +0200

    index

---
+++ about.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Energy-Drink</title>
+    <link rel="stylesheet" href="./css/about.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <section>
+        <nav>
+            <div class="navbar">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="buy.php">Buy</a></li>
+                    <li><a href="login.php">Login</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+    </section>
+
+    <div class="heading">
+        <h3>About us </h3>
+    </div>
+
+    <!-- about start -->
+    <section class="about">
+        <div class="row">
+            <div class="image">
+                <img src="./img/about.png" alt="">
+            </div>
+            <div class="content">
+                <h3>Why chose us ?</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, temporibus.Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, temporibus.</p>
+                <a href="buy.php" class="btn"> Our products</a>
+            </div>
+        </div>
+    </section>
+    <!-- about end -->
+    <!-- steps section starts -->
+
+<!-- voir brandon. SI j'enlève section et que je met div OK, sinon marche pas -->
+    <div class="steps">
+        <h1 class="title"> Simple steps</h1>
+
+        <div class="box-container">
+            <div class="box">
+                <img src="./img/step-1.png" alt="">
+                <h3> Choose order</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum, vero.</p>
+            </div>
+            <div class="box">
+                <img src="./img/step-2.png" alt="">
+                <h3> Fast delivery</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum, vero.</p>
+            </div>
+            <div class="box">
+                <img src="./img/step-3.png" alt="">
+                <h3> Fast delivery</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum, vero.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- steps section end  -->
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Energy-Drink</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <section>
+        <nav>
+            <div class="navbar">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="buy.php">Buy</a></li>
+                    <li><a href="login.php">Login</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="left">
+            <div class="bg">
+                <img src="img/canette.png">
+            </div>
+            <!-- <div class="play-btn">
+                <div class="play-btn-grand">
+                    <div class="play-btn-moyen">
+                        <div class="play-btn-petit">
+                        </div>
+                    </div>
+                </div>
+            </div> -->
+        </div>
+        <div class="right">
+            <div class="bg-right">
+                <img src="./img/bg-right.png" alt="">
+            </div>
+            <div class="particle">
+                <img src="./img/particle.png" alt="">
+            </div>
+            <div class="container">
+                <div class="box-up">
+                    <h1>Take Energy <br> just now</h1>  
+                    <div class="box-btn">
+                        <div class="detail">
+                            <button><a href="about.php">Detail</button></a>
+                        </div>
+                        <div class="buy">
+                            <button><a href="buy.php">Buy now</button></a>
+                        </div>
+                    </div>                               
+                    <div class="trait"></div>
+                    <div class="box-icon">
+                        <div class="icon">
+                            <img src="./svg/bolt.svg" alt="">
+                            <div class="paragraphe">
+                                <h2> Energy</h2>
+                                <p> Source of energy for most of life on Earth</p>
+                            </div>
+                        </div>
+                        <div class="icon">
+                            <img src="./svg/muscle.svg" alt="">
+                            <div class="paragraphe">
+                                <h2> Power </h2>
+                                <p> Rate of doing work, the amount of energy</p>
+                            </div>
+                        </div>
+                        <div class="icon">
+                            <img src="./svg/run.svg" alt="">
+                            <div class="paragraphe">
+                                <h2> Endurance</h2>
+                                <p> Upgrade your endurance, and become more fast</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="box-down">
+                    <ul>
+                        <li><a href="twitter">Twitter</a></li>
+                        <li><a href="twitter">Facebook</a></li>
+                        <li><a href="twitter">Instagram</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
